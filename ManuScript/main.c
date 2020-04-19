4734e11e0c39f0e1d0cb9f719611566ee1437da3
karaev andrey <karaevandrey2000@gmail.com>
2020-04-19 15:04:09 +0300

    variables 	modified:   ManuScript/lexer.h 	modified:   ManuScript/main.c

--- ManuScript/main.c
+++ ManuScript/main.c
@@ -1,8 +1,29 @@
 #include "lexer.h"
 #include "parser.h"
 
+struct variable {
+	int index;
+	char name[MAX_SIZE];
+	double value;
+};
+
+void newVariable(struct variable* var, char* name) {
+	strcpy_s(var->name, sizeof(var->name), name);
+	var->index = var->index++;
+}
+
 int main() {
 	char** tokens = 0;
+	struct variable variables[MAX_SIZE] = {1, "tyhtyh", 55.5, 2, "jutjy", 8.5};
+	for (struct variable* var = variables; var < variables + 2; var++)
+	{
+		printf("%lf\n", var->value);
+	}
+	struct variable vars;
+	vars.index = 1;
+	char name[MAX_SIZE] = "greg";
+	newVariable(&vars, &name);
+	printf("%d\n%s", vars.index, vars.name);
 	char buffer[MAX_SIZE] = { 0 };
 	while (gets_s(buffer, MAX_SIZE)) {
 		if (strlen(buffer) == 0) break;
